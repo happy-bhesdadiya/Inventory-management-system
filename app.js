a03664f7780224edfc3733999bc6fdcec96b8c85
bhargu-27 <bjkanodiya42@gmail.com>
2021-03-18 14:15:22 +0530

    path sorted

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 require('dotenv').config({path:'./credentials.env'})
 var sequelize= require('./connect');
 var Sequelize= require('sequelize');
-var branch_details = require('./models/branch');
-var product_details = require('./models/product');
-var product_mapping_details = require('./models/productMapping');
-var stock_details = require('./models/stock');
-var user_details = require('./models/user');
+var branch_details = require('./src/models/branch');
+var product_details = require('./src/models/product');
+var product_mapping_details = require('./src/models/productMapping');
+var stock_details = require('./src/models/stock');
+var user_details = require('./src/models/user');
 var app = express();
 
 app.use(express.json());
