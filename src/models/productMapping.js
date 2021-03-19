feff169b3f0a32807e36f92c9ed2991a366f8ba7
bhargav-bacancy-tech <bhargav.kanodiya@bacancy.com>
2021-03-19 17:22:03 +0530

    migrators and seeders are changed as per sequelize creates

--- src/models/productMapping.js
+++ src/models/productMapping.js
@@ -24,7 +24,7 @@ const productMapping=sequelize.define('productMapping',{
     }
 })
 
-user.hasOne(productMapping,{foriegnKey:user.id})
-branch.hasOne(productMapping,{foriegnKey:branch.id})
+user.hasOne(productMapping,{as:'assigned_by',foriegnKey:user.id})
+user.hasOne(productMapping,{as:'assigned_to',foriegnKey:user.id})
 product.hasOne(productMapping,{foriegnKey:product.id})
 module.exports=productMapping
